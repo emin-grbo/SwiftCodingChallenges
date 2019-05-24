cb418f31356838198b058ff3ee03e0ca179bba65
emin.roblack@gmail.com <emin.roblack@gmail.com>
2019-05-24 11:01:24 +0200

    Add second variation for first challenge

--- SCC.playground/Pages/Challenge1.xcplaygroundpage/Contents.swift
+++ SCC.playground/Pages/Challenge1.xcplaygroundpage/Contents.swift
@@ -7,6 +7,11 @@ func challenge1(input: String) -> Bool {
     return false
 }
 
+// v2.0
+func challenge1o(input: String) -> Bool {
+    return Set(input).count == input.count
+}
+
 assert(challenge1(input: "No duplicates") == true, "Challenge 1 failed")
 assert(challenge1(input: "abcdefghijklmnopqrstuvwxyz") == true, "Challenge 1 failed")
 assert(challenge1(input: "AaBbCc") == true, "Challenge 1 failed")
